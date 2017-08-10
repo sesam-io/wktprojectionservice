a5b7b737544135b68ab90530bb464ea7c47be463
Ashkan Vahidishams <ashkan.vahidishams@bouvet.no>
2017-08-10 10:28:45 +0200

    changing docker repo to sesambuild

--- docker/build.sh
+++ docker/build.sh
@@ -6,8 +6,8 @@ DOCKER_BUILD=${DOCKER_BUILD-1}
 DOCKER_BUILD_PUSH=${DOCKER_BUILD_PUSH-0}
 
 if [ $DOCKER_BUILD -eq 1 ]; then
-  docker build $DOCKER_BUILD_OPTIONS -t sesam/wktprojectionservice:$TAG .
+  docker build $DOCKER_BUILD_OPTIONS -t sesambuild/wktprojectionservice:$TAG .
 fi
 if [ $DOCKER_BUILD_PUSH -eq 1 ]; then
-  docker push sesam/wktprojectionservice:$TAG
+  docker push sesambuild/wktprojectionservice:$TAG
 fi
